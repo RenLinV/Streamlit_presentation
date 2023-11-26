b11ed3607a0e629011af95bcd9b8b3d604aac6f0
RenLinV <124538378+RenLinV@users.noreply.github.com>
2023-11-26 06:55:29 +0300

    Update main.py

--- pythonProject5/main.py
+++ pythonProject5/main.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
 st.markdown("# Hi")
-from st_pages import Page, Section, add_page_title, show_pages
-show_pages(
-        [
-            Page("main.py", "Main", "🏠"),
-            # Can use :<icon-name>: or the actual icon
-            Section(name="Docs")
-            Page("pages/cheat_sheet.py", "Cheat sheet", ":books:"),
+
+with st.echo("below"):
+        from st_pages import Page, Section, add_page_title, show_pages
+        show_pages(
+                [
+                        Page("main.py", "Main", "🏠"),
+                        # Can use :<icon-name>: or the actual icon
+                        Section(name="Docs"),
+                        Page("pages/cheat_sheet.py", "Cheat sheet", ":books:"),
             # Since this is a Section, all the pages underneath it will be indented
             # The section itself will look like a normal page, but it won't be clickable
             #Section(name="Cool features"),
             # The pages appear in the order you pass them
             #Page("Streamlit presentation/pages/Community.py", "Community"),
             #Page("Streamlit presentation/pages/Examples.py", "Examples"),
-        ]
-    )
+                ]
+        )
 
 #add_page_title()
