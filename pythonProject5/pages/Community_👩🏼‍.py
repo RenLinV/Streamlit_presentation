e2702acba9ba5cfc587489c6fb7a84466aa5a462
RenLinV <124538378+RenLinV@users.noreply.github.com>
2023-11-27 19:48:45 +0300

    Add files via upload

---
+++ pythonProject5/pages/Community_👩🏼‍.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.sidebar.header("**Community 👩🏼‍💻‍**")
+
+st.markdown("# **Community 👩🏼‍💻**")
+
+st.markdown('''
+Streamlit not only allow you to create web-apps, but also gives you support and opportunity to share your experience.
+
+Via ***[:green[Community Cloud]](https://streamlit.io/cloud)*** you can deploy, manage, and share your apps with the world, directly from Streamlit — 
+all for free. All you need to do is to:
+- sign in with Github or SSO
+- pick a repo, branch and file
+- click **Deploy**, then any time you do a *:violet[git push]* your app will update *immediately*
+
+
+***[:green[Forum]](https://discuss.streamlit.io/)*** allow you to connect with other app creators, ask questions and share your latest apps.
+
+
+***[:green[Components]](https://streamlit.io/components)*** are third-party modules that extend what’s possible with Streamlit. 
+Built by creators, for the community 🎈
+
+
+***[:green[Gallery]](https://streamlit.io/gallery)*** some of community's favorite apps created by Streamlit users and hosted on Streamlit Community 
+Cloud. 
+
+
+In ***[:green[Blog]](https://blog.streamlit.io/)*** you can find tutorials, products posts, Streamlit Github nd other useful things. On top of that, 
+you can become a streamlit advocate submitting your idea!
+''')
